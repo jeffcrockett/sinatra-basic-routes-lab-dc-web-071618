cebfcb7c0580f37db4cc400a93c6df5087af07cf
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-08-06 10:46:52 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        'Hello world!'
+    end
+
+    get '/name' do
+        'My name is Jeff'
+    end
+
+    get '/hometown' do 
+        'My hometown is Plano, TX'
+    end
+
+    get '/favorite-song' do
+        'My favorite song is ???'
+    end
 end
